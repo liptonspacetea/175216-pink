61f96d1e92003011325b1e247c14fdad52afde63
Antonov Konstantin <costa08@mail.ru>
2017-03-27 23:57:54 +0300

    страница главная финальная версия

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ function init(){
 
     var myPlacemark = new ymaps.Placemark([59.938631, 30.323055] , {},
         { iconLayout: 'default#image',
-          iconImageHref: '../img/icon-map-marker.svg',
+          iconImageHref: './img/icon-map-marker.svg',
           iconImageSize: [35, 35],
           iconImageOffset: [-20, -47] });
 
